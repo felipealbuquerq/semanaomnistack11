4a143dedd7fc1cdebc309d0e1d45343dbb009cc4
= <=>
2020-03-29 19:39:52 -0300

    Mobile da aplicação

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const routes = require('./routes');
-const cors = require('cors');
+const express = require('express')
+const cors = require('cors')
+const routes = require('./routes')
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
-app.use(routes);
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333);
